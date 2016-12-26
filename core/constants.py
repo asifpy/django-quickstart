6d7afe0325f8952f47eb09ebb5b1ca2cbec1d21f
Asif <saluasif@gmail.com>
2016-12-26 18:13:12 +0300

    added DRF to index

--- core/constants.py
+++ core/constants.py
@@ -1,5 +1,6 @@
 PACKAGES = [
-	('Django crudbuilder', 'core-post-list'),
-	('Crispy forms', 'crispy-view'),
-	('Django Addanother', 'addanother_example-player-list')
+    ('Django crudbuilder', 'core-post-list'),
+    ('Crispy forms', 'crispy-view'),
+    ('Django Addanother', 'addanother_example-player-list'),
+    ('Django REST framework', 'api-root')
 ]
